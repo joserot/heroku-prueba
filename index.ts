90f12456dd1bb8f4ab7613c983c60ba761c13f5b
joserot <jose.rotchen14@gmail.com>
2022-07-06 09:30:13 -0300

    dev.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as cors from "cors";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,8 +10,21 @@ app.use(
 	}),
 );
 
+const dev = process.env.NODE_ENV == "development";
 const port = process.env.PORT || 3500;
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/env", (req, res) => {
+	res.json({
+		environment: process.env.NODE_ENV,
+	});
+});
+
+app.get("/hello", (req, res) => {
+	res.json({
+		message: "hello, how are you?",
+	});
 });
